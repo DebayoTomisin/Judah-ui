7cb3972192546f9a8128efe9a613795f64d1da8c
DebayoTomisin <debayotomisin07@gmail.com>
2023-09-05 15:05:02 +0100

    Docs: update the config files for package to build

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import { peerDependencies, dependencies } from "./package.json";
-import plugin from "@vitejs/plugin-react-swc";
+
+import dts from "vite-plugin-dts";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        plugin({
-            jsxRuntime: "classic",
-        }),
-    ],
     resolve: {
         alias: {
             "@": resolve(__dirname, "src"),
@@ -19,17 +16,31 @@ export default defineConfig({
 
     build: {
         lib: {
-            entry: resolve(__dirname, "src", "index.ts"),
-            formats: ["es", "cjs"],
-            fileName: (ext) => `index.${ext}.js`,
+            entry: resolve(__dirname, "src/components/index.ts"),
+            name: "judahui",
+            formats: ["es", "cjs", "iife", "umd"],
+            fileName: "judahui",
         },
         rollupOptions: {
             external: [
                 ...Object.keys(peerDependencies),
                 ...Object.keys(dependencies),
             ],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
         },
         target: "esnext",
         sourcemap: true,
     },
+    plugins: [
+        react({
+            jsxRuntime: "classic",
+        }),
+        tsConfigPaths(),
+        dts({ include: ["src/components/"] }),
+    ],
 });
