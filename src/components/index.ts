74b7557d16a381f1c75287aa6c92f1c6cf42d2ea
DebayoTomisin <debayotomisin07@gmail.com>
2023-09-05 15:41:05 +0100

    Chore: Update the button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 import "tailwindcss/tailwind.css";
 
-export type { ButtonProps } from "./button/button";
+export type { ButtonProps } from "/@/components/button";
 
-export { Button } from "./button/button";
+export { Button } from "/@/components/button";
